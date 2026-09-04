cf6196d54edc6431a4a90b178b412228d34c250f
zoldrise <zoldikkx@proton.me>
2026-09-04 19:07:59 +0200

    Formatting ft_putchar.c

--- putChar/ft_putchar.c
+++ putChar/ft_putchar.c
@@ -1,13 +1,9 @@
 #include <unistd.h>
 
-void ft_putchar(char c)
-{
-    write(1, &c, 1);
-}
+void ft_putchar(char c) { write(1, &c, 1); }
 
-int main()
-{
-    char a = 'a';
-    ft_putchar(a);
-    return 0;
+int main() {
+  char a = 'a';
+  ft_putchar(a);
+  return 0;
 }
